e497923a530939723f09fbc6d0661922b297a916
Keagan-EXPLORE <keagan@explore-ai.net>
2022-02-24 08:13:26 +0200

    initial commit

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4; // make sure that this is the same as the version found in hardhat.config.js
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+      constructor() {
+            console.log("Check out my solidity contract!");
+      }
+}
